2a72e50ce1b0e49656e2b7f9d7b2b5ac535e9269
vench <nowwrit@mail.ru>
2014-11-12 01:25:11 +0400

    funct prioritet1

--- protected/controllers/ProjectController.php
+++ protected/controllers/ProjectController.php
@@ -28,11 +28,11 @@ class ProjectController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('index'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update', 'userProjectRmove'),
+				'actions'=>array('create','update', 'view', 'userProjectRmove'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
