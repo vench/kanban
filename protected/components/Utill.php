85aa4fcfcb22cca9973b95f4f20fd5b4bba2d213
vench <nowwrit@mail.ru>
2014-11-27 22:57:26 +0400

    x

--- protected/components/Utill.php
+++ protected/components/Utill.php
@@ -2,13 +2,25 @@
 
 class Utill {
 
-	public static function safeText($string) {
-		$match = NULL;
-		return preg_replace_callback(
+        /**
+         * 
+         * @param string $string
+         * @return string
+         */
+	public static function safeText($string) {	    
+		$string = preg_replace_callback(
             "/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/i",
               create_function( 
             '$matches',
             'return CHtml::encode($matches[0]);'),
             $string); 
+            return $string;
+                /*
+                return preg_replace_callback(
+            "/^[^>\"]+(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/i",
+              create_function( 
+            '$matches',
+            'return CHtml::link($matches[0], $matches[0], array("target"=>"_blank"));'),
+            $string); */
 	}
 }
