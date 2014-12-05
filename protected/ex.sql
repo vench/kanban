0b676bdcb517bb24b37857c0f2aa4a3da99306c4
vench <nowwrit@mail.ru>
2014-12-05 10:10:55 +0300

    Delete ex.sql

--- protected/ex.sql
+++
@@ -1,6 +0,0 @@
-SELECT t.new_category_id, SUM( t1.time_insert - t.time_insert ) AS s
-FROM  `tbl_task_history` AS t
-LEFT JOIN  `tbl_task_history` AS t1 ON ( t1.time_insert > t.time_insert ) 
-GROUP BY t.new_category_id
-ORDER BY t.time_insert
-LIMIT 0 , 30
