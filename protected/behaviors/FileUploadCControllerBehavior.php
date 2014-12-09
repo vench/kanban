5792239badae3542739c154bb724107f100e8d39
Raskin Venya <nowwrit@mail.ru>
2014-12-09 15:47:53 +0300

    fix 02

--- protected/behaviors/FileUploadCControllerBehavior.php
+++ protected/behaviors/FileUploadCControllerBehavior.php
@@ -25,8 +25,12 @@ class FileUploadCControllerBehavior extends CBehavior {
      
         $data = pathinfo($file->patch);
 		          
-		if(in_array(strtolower($data['extension']), array('jpg','jpeg','png','gif','txt',))) {
+		if(in_array(strtolower($data['extension']), array('jpg','jpeg','png','gif',))) {
             header("Content-type: image/{$data['extension']}");
+            echo file_get_contents($file->patch);
+        } else if(in_array(strtolower($data['extension']), array('txt',))) {
+           // header("Content-type: text/{$data['extension']}");
+		   header("Content-Type:text/plain");
             echo file_get_contents($file->patch);
         } else {
            header("Content-disposition: attachment; filename={$data['filename']}.{$data['extension']}"); 
