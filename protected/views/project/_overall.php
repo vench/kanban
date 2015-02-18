78210e7c3b2a104b30d051a7ad320646ec183692
Raskin Venya <nowwrit@mail.ru>
2015-02-18 08:58:41 +0300

    fix2

--- protected/views/project/_overall.php
+++ protected/views/project/_overall.php
@@ -9,7 +9,7 @@ $this->widget('zii.widgets.CDetailView', array(
 		array('name'=>'user_id', 'value'=>$model->user->name),		 
 		array('name'=>'description','type'=>'raw',), 
 		array(
-			'name'=>Yii::t('main', 'Only the main tasks'),
+			'label'=>Yii::t('main', 'Only the main tasks'),
 			'type'=>'raw',
 			'value'=>$showParent ? CHtml::link(Yii::t('main', 'No'), array('view', 'id'=>$model->id, 'only-parent'=>0,)) : CHtml::link(Yii::t('main', 'Yes'), array('view', 'id'=>$model->id, 'only-parent'=>1,))
 		),  
